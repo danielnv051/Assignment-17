84f5a68fe2c590fe44d0103c0f8719af8f5840bb
danielnv051 <danielnv0910@gmail.com>
2023-11-04 21:29:01 +0330

    complete assignment 17

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from PySide6.QtWidgets import QApplication
 from PySide6.QtUiTools import QUiLoader
 
-my_app = QApplication([])
 
+def test():
+    print('salam salam')
+
+
+my_app = QApplication([])
 loader = QUiLoader()
 my_window = loader.load("mainwindow.ui")
 my_window.show()
+
+my_window.pushButton.clicked.connect(test)
 
 my_app.exec()
