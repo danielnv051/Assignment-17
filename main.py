11158e0ab5dfa3e9149c33e0b148d5bc5bebf991
danielnv051 <danielnv0910@gmail.com>
2023-11-20 14:23:13 +0330

    complete assignment 17

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication
+""" from PySide6.QtWidgets import QApplication
 from PySide6.QtUiTools import QUiLoader
 
 app = QApplication([])
@@ -6,4 +6,17 @@ loader = QUiLoader()
 my_win = loader.load("my_win.ui")
 my_win.show()
 
-app.exec()
+app.exec_()
+ """
+
+from PySide6.QtWidgets import QApplication
+from PySide6.QtUiTools import QUiLoader
+
+my_app = QApplication([])
+
+loader = QUiLoader()
+window = loader.load("my_win.ui")
+window.show()
+
+my_app.exec()
+ 
